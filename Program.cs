17f6c682c8b828f6290645e3d1a04b9df73cde47
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-30 12:48:32 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+namespace Challenge_Labs_4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 25, 12, 43 };
+
+            var occurrenceDict = new Dictionary<int, int>();
+
+            for (int i = 0; i < myArray.Length; i++) 
+            {
+                if (occurrenceDict.ContainsKey(myArray[i])) 
+                {
+                    occurrenceDict[myArray[i]]+=1;
+                }
+                else 
+                {
+                    occurrenceDict.Add(myArray[i], 1);
+                }
+                
+
+            }
+            foreach (var item in occurrenceDict) 
+            {
+                Console.WriteLine($"{item.Key} occurs {item.Value} times");
+            }
+
+
+        }
+    }
+}
